d37ccd0ba4f8f4bab023b6b3ed93d9d83173b6eb
LilEntity <justlilproto@gmail.com>
2023-10-02 08:27:43 +0700

    convert to classes

---
+++ uni_assignments/TotalHarga.java
@@ -0,0 +1,33 @@
+package uni_assignments;
+import java.util.Scanner;
+
+public class TotalHarga {
+
+	public static void main(String[] args) {
+		long penghapus = 500;
+		long pensil = 1500;
+		long penggaris = 5500;
+		
+		Scanner scan = new Scanner(System.in);
+		String jenis = scan.next();
+		int jumlah = scan.nextInt();
+		
+		switch (jenis) {
+			case "penghapus":
+				jumlah *= penghapus;
+				break;
+			case "pensil":
+				jumlah *= pensil;
+				break;
+			case "penggaris":
+				jumlah *= penggaris;
+				break;
+			default:
+				System.out.println("Masukkan tidak sesuai");
+				jumlah = 0;
+		}
+		
+		System.out.println(jumlah);
+	}
+
+}
