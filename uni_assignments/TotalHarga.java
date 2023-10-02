91f8be05c2844482993d62e860f467e81e58dc81
LilEntity <justlilproto@gmail.com>
2023-10-02 08:34:09 +0700

    updet classes

--- uni_assignments/TotalHarga.java
+++ uni_assignments/TotalHarga.java
@@ -2,16 +2,15 @@ package uni_assignments;
 import java.util.Scanner;
 
 public class TotalHarga {
-
 	public static void main(String[] args) {
 		long penghapus = 500;
 		long pensil = 1500;
 		long penggaris = 5500;
-		
+
 		Scanner scan = new Scanner(System.in);
 		String jenis = scan.next();
 		int jumlah = scan.nextInt();
-		
+
 		switch (jenis) {
 			case "penghapus":
 				jumlah *= penghapus;
@@ -26,8 +25,7 @@ public class TotalHarga {
 				System.out.println("Masukkan tidak sesuai");
 				jumlah = 0;
 		}
-		
+
 		System.out.println(jumlah);
 	}
-
 }
